fe0b03ca5dd5e7ec3feb022f6d9294445a048dd3
Ninten7 <90481301+Nintenerd7@users.noreply.github.com>
2024-07-21 22:21:20 +0100

    enemy can follow the player

--- Scripts/Enemy_Operator.cs
+++ Scripts/Enemy_Operator.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Enemy_Operator : MonoBehaviour
 {
     public Enemy Enemy_Behaviour;
+    public Patrol move;
     //object orientation patrol
     // Start is called before the first frame update
     void Start()
     {
-        
+        Enemy_Behaviour = Enemy.patrol;
     }
 
     // Update is called once per frame
@@ -18,14 +19,20 @@ public class Enemy_Operator : MonoBehaviour
         switch(Enemy_Behaviour)
         {
           case Enemy.patrol:
+          move.Enemy_Patrol();
           //Access enemy patrol script
           break;
           case Enemy.Chase:
+          move.Chase();
           //Access enemy Chase script
           break;
           case Enemy.Attack:
           //Access enemy attack script
-          break;        
+          break;    
+
+          case Enemy.Return:
+          StartCoroutine(move.Return_To_Position());
+          break;     
         }
     }
 
@@ -33,5 +40,6 @@ public class Enemy_Operator : MonoBehaviour
         patrol,
         Chase,
         Attack,
+        Return,
     }
 }
---
+++ Scripts/Patrol.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    //variables
+    public float speed;
+    public Hearts_System attack;
+    public Enemy_Operator states;
+    public Transform Target;
+    public Transform returnPos;
+    //
+
+    void Start()
+    {
+      Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    //update
+    public void Enemy_Patrol()
+    {
+      speed = 0;//enemy stands still
+    }
+
+    public void Chase()
+    {
+      speed = 1.5f;//enemy kicks up speed 
+      transform.position = Vector2.MoveTowards(transform.position, Target.position,speed*Time.deltaTime);//enemy position will move towards player using the speed it has.
+    }
+
+    public IEnumerator Return_To_Position()
+    {
+
+      speed = 1f;//enemy slows speed to return back to zero
+      transform.position = Vector2.MoveTowards(transform.position, returnPos.position,speed*Time.deltaTime);//enemy position will move towards player using the speed it has.
+      yield return new WaitForSeconds(1);
+      states.Enemy_Behaviour = Enemy_Operator.Enemy.patrol;
+    }
+
+    public IEnumerator Attack(float charge_Speed)
+    {
+      //attack logic
+      yield break;
+    }
+}
