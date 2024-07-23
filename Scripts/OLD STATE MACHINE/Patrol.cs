48d366f4086e549246c4a295f0c0bf7cf6a1a501
Ninten7 <90481301+Nintenerd7@users.noreply.github.com>
2024-07-23 23:17:40 +0100

    Comments added to patrol script

--- Scripts/OLD STATE MACHINE/Patrol.cs
+++ Scripts/OLD STATE MACHINE/Patrol.cs
@@ -4,70 +4,70 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    //variables
-    public float[] CoolDown = new float [3];
     public float speed;
     public Enemy_Operator states;
     public Transform Target;
-    public Transform returnPos;
     public TrailRenderer tr;
     public Player_Detection PD;
 
     void Start()
     {
-      Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-      returnPos = GameObject.FindGameObjectWithTag("EnemyPos").GetComponent<Transform>();
-      Time.timeScale = 1f;
+      Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();//gets player tag for the Target transform position 
     }
 
-
+//STATE MACHINE ENUM: CALLS MOVING STATES WITH THE ALLOCATED PARAMETERS FOR THE TWO MAIN STATES BEING USED TO FOLLOW AND ATTACK THE PLAYER 
  public IEnumerator StateMachine()
  {
-      Moving(true, false);
-      yield return new WaitForSeconds(2f);
-      Moving(false, true);
-      yield return new WaitForSeconds(1f);
-      Moving(true, false);
-      PD.IsAttacking = false;
+      Moving(true, false);//Can chase but cannot attack
+      yield return new WaitForSeconds(2f);//chases player for two seconds
+      Moving(false, true);//Can attack but cannot move at normal speed
+      yield return new WaitForSeconds(1f);//attacks for one second 
+      Moving(true, false);//Can chase but attack is set back to false
+      PD.IsAttacking = false;//obtains Is attacking boolean from the Player_Detection Script so the coroutine can restart again
  }
-    //update
-    public void Idle()
-    {
-      speed = 0f;
-      Debug.Log("Idle");
-    }
+//END OF SEQUENCE
 
-public void AIFollow()
+//INDEX METHODS USED FOR THE ENUM STATES 
+
+ public void Idle()//IDLE: AI DOES NOT MOVE WHEN PLAYER IS NOT IN SIGHT 
 {
-    tr.emitting = false;
-    speed = 1f;
+  speed = 0f;//speed is null
+}
+
+public void AIFollow()//AI FOLLOW: 
+{
+    tr.emitting = false;//trail is not visible
+    speed = 1f;//speed is equal to one
     transform.position = Vector2.MoveTowards(transform.position, Target.position,speed*Time.deltaTime);//enemy position will move towards player using the speed it has.
 }
 public void AIAttack()
 {
-    tr.emitting = true;
-    speed = 3f;
+    tr.emitting = true;//trail is visible
+    speed = 3f;//speed is equal to three
     transform.position = Vector2.MoveTowards(transform.position, Target.position,speed*Time.deltaTime);//enemy position will move towards player using the speed it has.
 }
+
+//this method is used to check if the AI can move or attack
 public void Moving(bool CanMove, bool CanAttack)
 {
-
-  switch(CanMove)
+//CAN MOVE SWITCH STATEMENT
+  switch(CanMove)//switch statement used to switch states
   {
     case true:
-    states.Enemy_Behaviour = Enemy_Operator.Enemy.Chase;
+    states.Enemy_Behaviour = Enemy_Operator.Enemy.Chase;//if AI can move then AI can chase the player
     break;
     case false:
-    states.Enemy_Behaviour = Enemy_Operator.Enemy.Attack;
+    states.Enemy_Behaviour = Enemy_Operator.Enemy.Attack;//if AI can not move, AI will get aggressive
     break;
   }
-    switch(CanAttack)
+  //CAN ATTACK SWITCH STATEMENT
+    switch(CanAttack)//switch statement used to switch states
   {
     case true:
-    states.Enemy_Behaviour = Enemy_Operator.Enemy.Attack;
+    states.Enemy_Behaviour = Enemy_Operator.Enemy.Attack;//if AI can attack then AI can charge toward the player
     break;
     case false:
-    states.Enemy_Behaviour = Enemy_Operator.Enemy.Chase;
+    states.Enemy_Behaviour = Enemy_Operator.Enemy.Chase;//if AI can not attack, AI will return to the chase state.
     break;
   }
 
